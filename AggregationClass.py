d6c639462141876bf9ee636151c2fe803897d287
Dalnu <159875144+Dalnu@users.noreply.github.com>
2024-05-03 19:55:52 +0400

    Update AggregationClass.py

--- AggregationClass.py
+++ AggregationClass.py
@@ -1,197 +1,194 @@
 from InheritanceClass import *
+# Importing required classes or modules. 
 
 class Client(Person):
+# Defines a class `Client` inheriting from the `Person` class.
+
     """Represents a client of the company."""
+    # Docstring describing the purpose of the `Client` class.
 
     def __init__(self, clientID, name, address, contactDetails, budget):
         """Initialize a Client object with the given attributes."""
-        self._clientID = clientID
-        self._name = name
-        self._address = address
-        self._contactDetails = contactDetails
-        self._budget = budget
+        # Constructor method to initialize a `Client` object with specific attributes.
 
     # Getters
+    # Methods to get the values of attributes.
+
     def getClientID(self):
         """Return the client ID."""
-        return self._clientID
+        # Method to return the client ID.
 
     def getName(self):
         """Return the name of the client."""
-        return self._name
+        # Method to return the name of the client.
 
     def getAddress(self):
         """Return the address of the client."""
-        return self._address
+        # Method to return the address of the client.
 
     def getContactDetails(self):
         """Return the contact details of the client."""
-        return self._contactDetails
+        # Method to return the contact details of the client.
 
     def getBudget(self):
         """Return the budget of the client."""
-        return self._budget
+        # Method to return the budget of the client.
 
     # Setters
+    # Methods to set the values of attributes.
+
     def setClientID(self, clientID):
         """Set the client ID."""
-        self._clientID = clientID
+        # Method to set the client ID.
 
     def setName(self, name):
         """Set the name of the client."""
-        self._name = name
+        # Method to set the name of the client.
 
     def setAddress(self, address):
         """Set the address of the client."""
-        self._address = address
+        # Method to set the address of the client.
 
     def setContactDetails(self, contactDetails):
         """Set the contact details of the client."""
-        self._contactDetails = contactDetails
+        # Method to set the contact details of the client.
 
     def setBudget(self, budget):
         """Set the budget of the client."""
-        self._budget = budget
+        # Method to set the budget of the client.
 
 class Event:
+# Defines a class `Event`.
+
     """Represents an event organized by the company."""
+    # Docstring describing the purpose of the `Event` class.
 
     def __init__(self, eventID, type, theme, date, time, duration, venueAddress, clientID, cateringCompany, cleaningCompany, decorationsCompany, entertainmentCompany, furnitureSupplyCompany, invoice):
         """Initialize an Event object with the given attributes."""
-        self._eventID = eventID
-        self._type = type
-        self._theme = theme
-        self._date = date
-        self._time = time
-        self._duration = duration
-        self._venueAddress = venueAddress
-        self._clientID = clientID
-        self._guestList = []
-        self._cateringCompany = cateringCompany
-        self._cleaningCompany = cleaningCompany
-        self._decorationsCompany = decorationsCompany
-        self._entertainmentCompany = entertainmentCompany
-        self._furnitureSupplyCompany = furnitureSupplyCompany
-        self._invoice = invoice
+        # Constructor method to initialize an `Event` object with specific attributes.
 
     # Getters
+    # Methods to get the values of attributes.
+
     def getEventID(self):
         """Return the ID of the event."""
-        return self._eventID
+        # Method to return the ID of the event.
 
     def getType(self):
         """Return the type of the event."""
-        return self._type
+        # Method to return the type of the event.
 
     def getTheme(self):
         """Return the theme of the event."""
-        return self._theme
+        # Method to return the theme of the event.
 
     def getDate(self):
         """Return the date of the event."""
-        return self._date
+        # Method to return the date of the event.
 
     def getTime(self):
         """Return the time of the event."""
-        return self._time
+        # Method to return the time of the event.
 
     def getDuration(self):
         """Return the duration of the event."""
-        return self._duration
+        # Method to return the duration of the event.
 
     def getVenueAddress(self):
         """Return the address of the event venue."""
-        return self._venueAddress
+        # Method to return the address of the event venue.
 
     def getClientID(self):
         """Return the ID of the client."""
-        return self._clientID
+        # Method to return the ID of the client.
 
     def getGuestList(self):
         """Return the guest list for the event."""
-        return self._guestList
+        # Method to return the guest list for the event.
 
     def getCateringCompany(self):
         """Return the catering company for the event."""
-        return self._cateringCompany
+        # Method to return the catering company for the event.
 
     def getCleaningCompany(self):
         """Return the cleaning company for the event."""
-        return self._cleaningCompany
+        # Method to return the cleaning company for the event.
 
     def getDecorationsCompany(self):
         """Return the decorations company for the event."""
-        return self._decorationsCompany
+        # Method to return the decorations company for the event.
 
     def getEntertainmentCompany(self):
         """Return the entertainment company for the event."""
-        return self._entertainmentCompany
+        # Method to return the entertainment company for the event.
 
     def getFurnitureSupplyCompany(self):
         """Return the furniture supply company for the event."""
-        return self._furnitureSupplyCompany
+        # Method to return the furniture supply company for the event.
 
     def getInvoice(self):
         """Return the invoice for the event."""
-        return self._invoice
+        # Method to return the invoice for the event.
 
     # Setters
+    # Methods to set the values of attributes.
+
     def setEventID(self, eventID):
         """Set the ID of the event."""
-        self._eventID = eventID
+        # Method to set the ID of the event.
 
     def setType(self, type):
         """Set the type of the event."""
-        self._type = type
+        # Method to set the type of the event.
 
     def setTheme(self, theme):
         """Set the theme of the event."""
-        self._theme = theme
+        # Method to set the theme of the event.
 
     def setDate(self, date):
         """Set the date of the event."""
-        self._date = date
+        # Method to set the date of the event.
 
     def setTime(self, time):
         """Set the time of the event."""
-        self._time = time
+        # Method to set the time of the event.
 
     def setDuration(self, duration):
         """Set the duration of the event."""
-        self._duration = duration
+        # Method to set the duration of the event.
 
     def setVenueAddress(self, venueAddress):
         """Set the address of the event venue."""
-        self._venueAddress = venueAddress
+        # Method to set the address of the event venue.
 
     def setClientID(self, clientID):
         """Set the ID of the client."""
-        self._clientID = clientID
+        # Method to set the ID of the client.
 
     def setGuestList(self, guestList):
         """Set the guest list for the event."""
-        self._guestList = guestList
+        # Method to set the guest list for the event.
 
     def setCateringCompany(self, cateringCompany):
         """Set the catering company for the event."""
-        self._cateringCompany = cateringCompany
+        # Method to set the catering company for the event.
 
     def setCleaningCompany(self, cleaningCompany):
         """Set the cleaning company for the event."""
-        self._cleaningCompany = cleaningCompany
+        # Method to set the cleaning company for the event.
 
     def setDecorationsCompany(self, decorationsCompany):
         """Set the decorations company for the event."""
-        self._decorationsCompany = decorationsCompany
+        # Method to set the decorations company for the event.
 
     def setEntertainmentCompany(self, entertainmentCompany):
         """Set the entertainment company for the event."""
-        self._entertainmentCompany = entertainmentCompany
+        # Method to set the entertainment company for the event.
 
     def setFurnitureSupplyCompany(self, furnitureSupplyCompany):
         """Set the furniture supply company for the event."""
-        self._furnitureSupplyCompany = furnitureSupplyCompany
+        # Method to set the furniture supply company for the event.
 
     def setInvoice(self, invoice):
         """Set the invoice for the event."""
-        self._invoice = invoice
+        # Method to set the invoice for the event.
